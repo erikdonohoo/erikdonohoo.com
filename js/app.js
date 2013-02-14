09ab41089fd391aa2193da9ee1c01ff03c0c6a5f
Erik Donohoo <erikdonohoo@gmail.com>
2013-02-14 09:46:30 -0700

    + Added angular.js pieces for navigation

---
+++ js/app.js
@@ -0,0 +1,51 @@
+//*****************************************************************************
+//  ANGULAR MODULE SETUP
+//*****************************************************************************
+
+var app = angular.module('app', []);
+app.config(['$routeProvider', function($routeProvider,$locationProvider) {
+  $routeProvider.
+  	  when('/', {templateUrl: 'html/home.html', controller: HomeCtrl}).
+  	  when('/games', {templateUrl: 'html/games.html', controller: GamesCtrl}).
+  	  when('/about', {templateUrl: 'html/about.html', controller: AboutCtrl}).
+      otherwise({redirectTo: '/'});
+}]);
+
+//*****************************************************************************
+//  ROOTSCOPE SETUP
+//*****************************************************************************
+app.run(function($rootScope) {
+ 	
+ 	// Page name
+ 	$rootScope.pageName = '';
+ 	$rootScope.getPageName = function() {
+ 		return $rootScope.pageName;
+ 	}
+ 	$rootScope.setPageName = function(name) {
+ 		$rootScope.pageName = name;
+ 	}
+});
+
+//*****************************************************************************
+//  HOME CTRL
+//*****************************************************************************
+function HomeCtrl($scope, $routeParams, $location) {
+
+	$scope.setPageName('home');
+}
+
+//*****************************************************************************
+//  GAMES CTRL
+//*****************************************************************************
+function GamesCtrl($scope, $routeParams, $location) {
+
+	$scope.setPageName('games');
+}
+
+//*****************************************************************************
+//  ABOUT CTRL
+//*****************************************************************************
+function AboutCtrl($scope, $routeParams, $location) {
+
+	$scope.setPageName('about');
+}
